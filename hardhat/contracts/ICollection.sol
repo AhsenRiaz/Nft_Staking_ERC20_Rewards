8d68a6dacb19f7cba6bbe88268410557785b7e34
ahsenrz <ahsenriaz2001@gmail.com>
2022-07-18 04:32:21 +0500

    Create ICollection.sol

---
+++ hardhat/contracts/ICollection.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
+interface ICollection is IERC721Enumerable {
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    function mint(address to_, uint256 mintAmount_) external payable;
+
+    function walletOfOwner(address owner_)
+        external
+        view
+        returns (uint256[] calldata);
+
+    function tokenURI(uint256 tokenId) external view returns (string calldata);
+
+    function setMaxMintAmount(uint256 _newMaxMintAmount) external;
+
+    function setBaseURI(string calldata _newBaseURI) external;
+
+    function setBaseExtension(string calldata _newExtension) external;
+
+    function pause(bool _state) external;
+
+    function withdraw() external payable;
+
+    // IOwner
+
+    function owner() external view returns (address);
+
+    function renounceOwnership() external;
+
+    function transferOwnership(address newOwner) external;
+
+    function _transferOwnership(address newOwner) external;
+}
