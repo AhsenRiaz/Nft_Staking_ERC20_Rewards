abb94397597f5bfbb5efa57ac933fad1b57182dd
ahsenrz <ahsenriaz2001@gmail.com>
2022-07-18 04:32:28 +0500

    Create IERC20Rewards.sol

---
+++ hardhat/contracts/IERC20Rewards.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT LICENSE
+pragma solidity 0.8.9;
+
+interface IERC20Rewards {
+    function mint(address to, uint256 amount) external;
+
+    function burnFrom(address account, uint256 amount) external;
+
+    function addController(address controller) external;
+
+    function removeController(address controller) external;
+}
